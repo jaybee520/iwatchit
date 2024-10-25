1aafd1e7444c8a710c83a1be3e1946cd509c7eeb
Jaybee <8266168+jaybee@user.noreply.gitee.com>
2024-10-25 10:35:52 +0800

    update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from math import pow, sqrt
 from mss import mss
 import numpy as np
 
-from win32 import win32api
-import win32con
+# from win32 import win32api
+# import win32con
 import pyautogui
 import lib.viz as viz
 from timer import Timer
@@ -18,8 +18,8 @@ if __debug__:
 
 # The size of the window to scan for targets in, in pixels
 # i.e. SQUARE_SIZE of 600 => 600 x 600px
-SQUARE_SIZE = 1400
-viz.SQUARE_SIZE = SQUARE_SIZE
+SQUARE_SIZE = 350
+viz.SQUARE_SIZE = 100
 
 # The maximum possible pixel distance that a character's center
 # can be before locking onto them
@@ -37,28 +37,12 @@ dimensions = sct.monitors[1]
 # Compute the center square of the screen to parse
 # dimensions['left'] = int((dimensions['width'] / 2) - (SQUARE_SIZE / 2))
 # dimensions['top'] = int((dimensions['height'] / 2) - (SQUARE_SIZE / 2))
-dimensions['left'] = 0
-dimensions['top'] = 0
-dimensions['width'] = 710
-dimensions['height'] = SQUARE_SIZE
+dimensions['left'] = 1000
+dimensions['top'] = 50
+dimensions['width'] = 370
+dimensions['height'] = 720
 
 print(dimensions, flush=True)
-
-# Calls the Windows API to simulate mouse movement events that are sent to OW
-def mouse_move(x, y):
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
-
-def mouse_click(x,y):
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-# Determines if the Caps Lock key is pressed in or not
-def is_activated():
-    return win32api.GetAsyncKeyState(0x14) != 0
-
-
-# def r(num, rand):
-#     return num + rand * random.random()
 
 def click_image(image, pos, action, timestamp, offset=5):
     pyautogui.FAILSAFE = False
@@ -73,107 +57,72 @@ def click_image(image, pos, action, timestamp, offset=5):
     pyautogui.moveTo(point_width,point_height, timestamp)
     # time.sleep(random())
     pyautogui.leftClick()
-    # mouse_click(point_width,point_height)
-    # mouse_click(1,100)
     print(f'point_height: {point_height}, point_width: {point_width}', flush=True)
     return True
 
 # Main lifecycle
-
-
-# 预约天猫茅台
-# steps = [
-#     {
-#         "image" : 'tmall_images/open_taobao.png',
-#         "desc" : "打开淘宝",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/my_taobao.png',
-#         "desc" : "进入我的淘宝",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/my_favorites.png',
-#         "desc" : "进入我的收藏",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/select_maotai.png',
-#         "desc" : "选择茅台",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/yuyue_maotai.png',
-#         "desc" : "预约茅台",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/yuyue_chenggong.png',
-#         "desc" : "预约成功",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/taobao_tab.png',
-#         "desc" : "淘宝标签页",
-#         "post_action": "click_random",
-#         "timeout": 5
-#     },
-#     {
-#         "image" : 'tmall_images/close_taobao.png',
-#         "desc" : "关闭淘宝",
-#         "post_action": "click_random",
-#         "timeout": 2.5
-#     },
-# ]
+# 预约京东
 steps = [
     {
-        "image" : 'tmall_images/maotai_shoppingcar.png',
-        "desc" : "进入茅台购物车",
+        "image" : 'jd_images/launcher_jd.png',
+        "desc" : "打开JD",
         "post_action": "click_random",
-        "timeout": 5
+        "timeout": 0.1
     },
     {
-        "image" : 'tmall_images/select_all_shoppingcar.png',
-        "desc" : "勾选购物车",
+        "image" : 'jd_images/launcher_jd.png',
+        "desc" : "打开JD",
         "post_action": "click_random",
-        "timeout": 5
+        "timeout": 2
     },
     {
-        "image" : 'tmall_images/jiesuan_shoppingcat.png',
-        "desc" : "结算购物车",
-        "post_action": "wait_click_center",
-        "timeout": 5
+        "image" : 'jd_images/my.png',
+        "desc" : "进入我的",
+        "post_action": "click_random",
+        "timeout": 2
     },
     {
-        "image" : 'tmall_images/select_alipay.png',
-        "desc" : "勾选支付宝支付",
-        "post_action": "click_random",
-        "timeout": 5
+        "image" : 'jd_images/feav.png',
+        "desc" : "进入我的收藏",
+        "post_action": "click_center",
+        "timeout": 2
     },
     {
-        "image" : 'tmall_images/tijiaodingdan.png',
-        "desc" : "提交订单",
+        "image" : 'jd_images/maotai.png',
+        "desc" : "选择茅台",
         "post_action": "click_random",
-        "timeout": 5
+        "timeout": 2
     },
+    {
+        "image" : 'jd_images/rightnow_appointment.png',
+        "desc" : "立即预约",
+        "post_action": "click_random",
+        "timeout": 1
+    },
+    {
+        "image" : 'jd_images/appointment_sucess.png',
+        "desc" : "预约结果",
+        "post_action": "save_screen",
+        "timeout_action": "save_screen",
+        "timeout": 1
+    }
 ]
 def start_tmall():
     timers = Timer()
     timers.start()
 
 def check_switch(old_screen):
+    time_start = time.process_time()
     while True: 
         frame = np.asarray(sct.grab(dimensions))
-        if viz.check_switch_screen(old_screen=old_screen, new_screen=frame, precision=0.2):
-            return True
-        time.sleep(0.3)
+        if __debug__:
+            cv2.imshow('res', frame)
+        if (time.process_time() - time_start) < val["timeout"]:
+            if viz.check_switch_screen(old_screen=old_screen, new_screen=frame, precision=0.45):
+                return True
+            time.sleep(0.1)
+        else:
+            return False
 
 def search_image(image):
     time_start = time.process_time()
@@ -198,40 +147,41 @@ def search_image(image):
         if cv2.waitKey(25) & 0xFF == ord("q"):
                 sys.exit(1)
         
-        time.sleep(0.3)
+        time.sleep(0.1)
 
 for key, val in enumerate(steps):
     
     print(f'开始执行步骤: {key} , {val}', flush=True)
 
     search = cv2.imread( val["image"] )
-    cv2.imshow('img', search)
+    # cv2.imshow('img', search)
     is_search, screen, center_point, random_point = search_image(val["image"])
+
     # # 查到图片后操作
     print(f'is_search: {is_search}', flush=True)
     # print(f'val.get("post_action"): {val.get("post_action")}', flush=True)
     print(f'len(val["post_action"]): {len(val["post_action"])}', flush=True)
     if is_search == True:
-        if val.get("post_action"):
-            if len(val["post_action"]) > 1:
-                print(f'开始执行动作{val["post_action"]}', flush=True)
-                # 休眠2s内任意数
-                time.sleep(random.random() * 2)
-                if val["post_action"] == 'click_center':
-                    is_move = click_image(screen, center_point, "left", 0.2, offset=5)
-                if val["post_action"] == 'click_random':
-                    is_move = click_image(screen, random_point, "left", 0.2, offset=5)
-                if val["post_action"] == 'wait_click_center':
-                    start_tmall()
-                    is_move = click_image(screen, random_point, "left", 0.2, offset=5)
-                print('开始验证点击')
-                check_switch(screen)
-                print('验证通过')
+        if val.get("post_action") and len(val["post_action"]) > 1:
+            print(f'开始执行动作{val["post_action"]}', flush=True)
+            # 休眠2s内任意数
+            time.sleep(random.random() * 2)
+            if val["post_action"] == 'click_center':
+                is_move = click_image(screen, center_point, "left", 0.2, offset=5)
+            if val["post_action"] == 'click_random':
+                is_move = click_image(screen, random_point, "left", 0.2, offset=5)
+            if val["post_action"] == 'wait_click_center':
+                start_tmall()
+                is_move = click_image(screen, random_point, "left", 0.2, offset=5)
+            print('开始验证点击')
+            check_switch(screen)
+            print('验证通过')
     
     # 在超时时间内没有找到图片
     else:
-        if val.get("timeout_action"):
-            pass
+        if val.get("timeout_action") and len(val["timeout_action"]) > 1:
+            if val["timeout_action"] == 'save_screen':
+                cv2.imwrite('failed.png', screen)
 
 
 sct.close()
